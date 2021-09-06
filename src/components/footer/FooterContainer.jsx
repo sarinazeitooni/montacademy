013abf842a6b4fc51d996b15853ecc72c6084b0d
sarinazeitooni <zeitoonisarina@gmail.com>
2021-09-06 11:22:27 +0430

    added footer without css

--- src/components/footer/FooterContainer.jsx
+++ src/components/footer/FooterContainer.jsx
@@ -1,9 +1,42 @@
 import React from "react";
-import './styles/footer.css';
-const FooterContainer = ()=>{
-    return(
-        <>
-        </>
+import './styles/footer.scss';
+import FooterData from "./data/data";
+
+const FooterContainer = () => {
+    return (
+        <div className='footer-container'>
+            <div className='columns-container'>
+                <div className='column'>
+                    <img src='https://www.monta.ir/landings/Footer//Monta_Footer.svg'/>
+                </div>
+                {
+                    FooterData.columns.map((item) => {
+                        return (
+                            <div className="column">
+                                <div className='column-title'>{item.title}</div>
+                                {item.links && <div className='links-container'>
+                                    {item.links.map((link) => {
+                                        return (
+                                            <div className='link'>{link}</div>
+                                        )
+                                    })}
+                                </div>}
+                                {item.icons && <div className='icons-container'>
+                                    {item.icons.map((icon)=>{
+                                        return(
+                                            <img src={icon}/>
+                                        )
+                                    })}
+                                </div> }
+                            </div>
+                        )
+                    })
+                }
+                <div className='column'>
+                    <img src={FooterData.Enamad}/>
+                </div>
+            </div>
+        </div>
     )
 };
 export default FooterContainer;
---
+++ src/components/footer/data/data.js
@@ -0,0 +1,32 @@
+const FooterData = {
+    columns : [
+        {
+            title: 'راه های ارتباطی',
+
+        },
+        {
+            title: 'لینک های مفید',
+            links : [
+                'منتآکادمی',
+                'موقعیت‌های شغلی',
+                'منتآزمون',
+                'جذب نمایندگی',
+                'پلتفرم کلاس آنلاین'
+            ]
+
+        },
+        {
+            title: 'ما را دنبال کنید!',
+            icons : [
+                'https://www.monta.ir/landings/Footer//Instagram.svg',
+                'https://www.monta.ir/landings/Footer//telegram.svg',
+                'https://www.monta.ir/landings/Footer//aparat.svg',
+                'https://www.monta.ir/landings/Footer//Linkedin.svg'
+            ]
+
+        }
+    ],
+    Enamad : 'https://www.monta.ir/landings/Footer//Enamad.PNG'
+    }
+;
+export default FooterData;
